94bc89c80419ad9f99ec181836ff38b1f8918a34
Fantx Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-05-04 07:38:00 +0800

    Optimize a little bit

--- raii.h
+++ raii.h
@@ -4,13 +4,13 @@
 
 // Typedefs
 typedef void (*RAII_H_FINAL)(void*);
-typedef struct __RHL { void** final_which; RAII_H_FINAL final; }  RAII_H_LAYER;
+typedef struct __RHL { void** final_which; RAII_H_FINAL final; }   RAII_H_LAYER;
 typedef struct __RHC { RAII_H_LAYER layer; struct __RHC* parent; } RAII_H_CHAIN;
 
 // Utils
 static RAII_H_CHAIN* RAII_H_CHAIN_CURRENT = 0;
 inline void* RAII_H_GET_EXITER( void** final_which, RAII_H_FINAL final ) {
-    RAII_H_CHAIN* latest = (RAII_H_CHAIN*)malloc(24);
+    RAII_H_CHAIN* latest = (RAII_H_CHAIN*)malloc(sizeof(RAII_H_CHAIN));
     ((RAII_H_LAYER*)latest)->final_which = final_which;   // 1st member, convert the ptr directly
     ((RAII_H_LAYER*)latest)->final = final;   // 1st member, convert the ptr directly
     latest->parent = RAII_H_CHAIN_CURRENT;
@@ -18,7 +18,7 @@ inline void* RAII_H_GET_EXITER( void** final_which, RAII_H_FINAL final ) {
     return (void*)1;
 }
 inline void RAII_H_FINAL_ONCE() {
-    (*(((RAII_H_LAYER*)RAII_H_CHAIN_CURRENT)->final))(*(((RAII_H_LAYER*)RAII_H_CHAIN_CURRENT)->final_which));
+    (*(((RAII_H_LAYER*)RAII_H_CHAIN_CURRENT)->final))(*(void**)RAII_H_CHAIN_CURRENT);   // 1st member, convert the ptr directly
     RAII_H_CHAIN* RAII_H_TEMPORARY_PARENT = RAII_H_CHAIN_CURRENT->parent;
     free(RAII_H_CHAIN_CURRENT);
     RAII_H_CHAIN_CURRENT = RAII_H_TEMPORARY_PARENT;
